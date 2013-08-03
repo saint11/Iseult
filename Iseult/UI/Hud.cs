6845e18f201e7621d252f40e907e74fdb5274f33
saint11 <saintjust@gmail.com>
2013-08-02 21:48:03 -0300

    Barricates and levels

--- Iseult/UI/Hud.cs
+++ Iseult/UI/Hud.cs
@@ -12,21 +12,39 @@ namespace Iseult
     public class Hud: PlatformLevelEntity
     {
         private Sprite<string> EquipedItem;
+        private Sprite<string> MordecaiStats;
 
         public Hud()
             : base(GameLevel.HUD_LAYER)
         {
-            Add(new Image(IseultGame.Atlas["hud/hudBg"]));
-            Position = new Vector2(10);
+            Image bg;
+            Add(bg = new Image(IseultGame.Atlas["hud/hudBg"]));
+            bg.Position = new Vector2(1000, 10);
 
             EquipedItem = IseultGame.SpriteData.GetSpriteString("item");
-            EquipedItem.Position = new Vector2(16);
+            EquipedItem.Position = new Vector2(1020,20);
             Add(EquipedItem);
+
+            MordecaiStats = IseultGame.SpriteData.GetSpriteString("mordecaiStats");
+            MordecaiStats.Position = new Vector2(10);
+            Add(MordecaiStats);
         }
 
         public override void Step()
         {
             base.Step();
+            if (Mordecai.Instance != null)
+            {
+                string target="idle";
+                if (Mordecai.Instance.Target==null) target = "idle";
+                else {
+                    if (Mordecai.Instance.Target is IseultPlayer) target = "follow";
+                    else target = "followOther";
+                }
+                MordecaiStats.Play(target);
+
+            }
+
             if (IseultPlayer.Carrying != null)
             {
                 EquipedItem.Play(IseultPlayer.Carrying.ItemName);
